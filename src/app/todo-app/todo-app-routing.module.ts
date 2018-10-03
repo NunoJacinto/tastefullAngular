236612460a196d28b435fcec194060d3d078e74c
Nuno Jacinto <nuno.jacinto@innowave.tech>
2018-10-03 14:42:47 +0100

    todoApp step 8.4 with an error

--- src/app/todo-app/todo-app-routing.module.ts
+++ src/app/todo-app/todo-app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TodoAppComponent } from './todo-app.component';         // add
+import { TodoItemComponent } from './todo-item/todo-item.component';   
+
 
 const routes: Routes = [{                                   // add
     path: '',                                               // add
-    component: TodoAppComponent,                              // add
+    component: TodoAppComponent,   
+    children: [
+      { path: ':index', component: TodoItemComponent },   
+    ]
+  
 }];        
 
 
