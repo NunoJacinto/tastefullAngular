2533ee4045c485c888e2c76dcde4792fe45e7b76
Nuno Jacinto <nuno.jacinto@innowave.tech>
2018-10-03 10:38:41 +0100

    step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '@env/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasteful-angular';
+
+  constructor(){
+    console.log({environment});
+  }
 }
